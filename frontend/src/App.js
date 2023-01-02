2422d9536bafb6368d90eea7e0d00ae193871e63
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-03 03:41:15 +0500

    1:41:46 completed contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,29 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/app.scss";
 import Header from "./components/layouts/Header";
 import Home from "./components/home/home";
+import Contact from "./components/Contact/Contact";
 import "./styles/header.scss";
 import "./styles/home.scss";
 import "./styles/founder.scss";
 import "./styles/footer.scss";
 import "./styles/menu.scss";
+import "./styles/contact.scss";
 function App() {
-    return (
-      <div className="App">
-        <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />}>
-              {" "}
-            </Route>{" "}
-          </Routes>{" "}
-        </Router>{" "}
-      </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}>
+            {" "}
+          </Route>{" "}
+          <Route path="/contact" element={<Contact />}>
+            {" "}
+          </Route>{" "}
+        </Routes>{" "}
+      </Router>{" "}
+    </div>
+  );
 }
 
 export default App;
