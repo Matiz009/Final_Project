6bcd66076aea549033a18c29107def1a74f71142
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-04 23:58:32 +0500

    2:05

--- frontend/src/components/Cart/Shipping.jsx
+++ frontend/src/components/Cart/Shipping.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 const Shipping = () => {
-  return <div>Shipping</div>;
+  return (
+    <div>
+      <section className="shipping">
+        <main>
+          <h1>Shipping Details</h1>
+          <form action="">
+            <div>
+              <label>House Number</label>
+              <input type="text" placeholder="Enter House No." />
+            </div>
+            <div>
+              <label>City</label>
+              <input type="text" placeholder="Enter City" />
+            </div>
+            <div>
+              <label>Pin Code</label>
+              <input type="number" placeholder="Enter Pin" />
+            </div>
+            <div>
+              <label>Phone Number</label>
+              <input type="number" placeholder="Enter Phone Number" />
+            </div>
+          </form>
+        </main>
+      </section>
+    </div>
+  );
 };
 
 export default Shipping;
