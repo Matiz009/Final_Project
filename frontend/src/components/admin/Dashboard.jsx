c40c99ed4224fc25d5269ae407a02878522efe36
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-05 18:01:57 +0500

    3:30 completed

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const Box = ({ title, value }) => (
+  <div>
+    <h3>
+      {title === "Income"}
+      {value}
+    </h3>
+    <p>{title}</p>
+  </div>
+);
 
 const Dashboard = () => {
   return (
-    <div>
-      <section className="dashboard">
-        <main>
-          <article></article>
-          <section></section>
-        </main>
-      </section>
-    </div>
+    <section className="dashboard">
+      <main>
+        <article>
+          <Box title="Users" value={213} />
+          <Box title="Orders" value={23} />
+          <Box title="Income" value={21323} />
+        </article>
+
+        <section>
+          <div>
+            <Link to="/admin/orders">View Orders</Link>
+            <Link to="/admin/users">View Users</Link>
+          </div>
+        </section>
+      </main>
+    </section>
   );
 };
 
