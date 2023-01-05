8d8c6e04bc159758401c1717269f1720660288ee
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-05 18:55:07 +0500

    3:35

--- frontend/src/components/admin/Dashboard.jsx
+++ frontend/src/components/admin/Dashboard.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, Tooltip, ArcElement, Legend } from "chart.js";
 
+ChartJS.register(Tooltip, ArcElement, Legend);
 const Box = ({ title, value }) => (
   <div>
     <h3>
@@ -12,6 +15,22 @@ const Box = ({ title, value }) => (
 );
 
 const Dashboard = () => {
+  const data = {
+    labels: ["Preparing", "Shipped", "Delivered"],
+    datasets: [
+      {
+        label: "# of orders",
+        data: [2, 3, 4],
+        backgroundColor: [
+          "rgba(159,63,176,0.1)",
+          "rgba(78,63,176,0.2)",
+          "rgba(156,0,60,0.3)",
+        ],
+        borderColor: ["rgb(159,63,176)", "rgb(78,63,176)", "rgb(156,0,60)"],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <section className="dashboard">
       <main>
@@ -26,6 +45,9 @@ const Dashboard = () => {
             <Link to="/admin/orders">View Orders</Link>
             <Link to="/admin/users">View Users</Link>
           </div>
+          <aside>
+            <Doughnut data={data} />
+          </aside>
         </section>
       </main>
     </section>
