99894aab452b0707ac517adb6c1c583e391378c9
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-07 00:05:31 +0500

    32:30 completed

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectPassport } from "./utils/Provider.js";
 const app = express();
 
 export default app;
 dotenv.config({
     path: "./config/config.env",
 });
-
+connectPassport();
 //importing routes
-import usersRoute from "./routes/users";
+import usersRoute from "./routes/users.js";
 app.use("/api/v1", usersRoute);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,4 +3,20 @@ import passport from "passport";
 
 const router = express.Router();
 
+router.get(
+    "/googlelogin",
+    passport.authenticate("google", {
+        scope: ["profile"],
+    })
+);
+
+router.get(
+    "/login",
+    passport.authenticate("google", {
+        scope: ["profile"],
+        successRedirect: process.env.FRONTEND_URL,
+    })
+);
+
+
 export default router;
