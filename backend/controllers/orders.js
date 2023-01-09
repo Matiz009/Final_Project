81675bd7bef62044264e6168f761ebfa25a01b39
MATI UL REHMAN <iammatiz00@gmail.com>
2023-01-10 00:47:30 +0500

    1:27

--- backend/controllers/orders.js
+++ backend/controllers/orders.js
@@ -1,5 +1,6 @@
 import { asyncError } from "../middlewares/errorMiddleware.js";
 import { OrderModel } from "../models/OrderModel.js";
+import ErrorHandler from "../utils/ErrorHandler.js";
 export const placeOrder = asyncError(async(req, res, next) => {
     const {
         shippingInfo,
@@ -25,5 +26,27 @@ export const placeOrder = asyncError(async(req, res, next) => {
     res.status(201).json({
         success: true,
         message: "Order Placed Successfully using COD",
+    });
+});
+export const getMyOrders = asyncError(async(req, res, next) => {
+    const orders = await OrderModel.find({
+        user: req.user._id,
+    }).populate("user", "name");
+
+    res.status(200).json({
+        success: true,
+        orders,
+    });
+});
+
+export const getOrderDetails = asyncError(async(req, res, next) => {
+    const order = await OrderModel.findById(req.params.id).populate(
+        "user",
+        "name"
+    );
+    if (order) return next(new ErrorHandler("invalid order id", 404));
+    res.status(200).json({
+        success: true,
+        order,
     });
 });
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { placeOrder } from "../controllers/orders.js";
+import {
+    getMyOrders,
+    getOrderDetails,
+    placeOrder,
+} from "../controllers/orders.js";
 const router = express.Router();
 router.post("/createOrder", placeOrder);
+router.get("/myOrders", isAuthenticated, getMyOrders);
+router.get("/order/:id", isAuthenticated, getOrderDetails);
 export default router;
